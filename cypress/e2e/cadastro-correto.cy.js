fd1a3d2b0f0b637149d673975100cd273873ece2
Luciano Bohnert <luciano.bohnert@gmail.com>
2025-08-05 15:56:40 -0300

    Primeiro commit do projeto curso-cypress

---
+++ cypress/e2e/cadastro-correto.cy.js
@@ -0,0 +1,34 @@
+describe('Página de Cadastro', () => {
+  it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="input-name"]').type('Ana de Jesus');
+    cy.get('[data-test="input-email"]').type('ana@email.com');
+    cy.get('[data-test="input-password"]').type('Senha123');
+    cy.get('[data-test="input-confirm-password"]').type('Senha123');
+    cy.get('[data-test="submit-button"]').click();
+  })
+  /*
+  it('deve carregar a página corretamente e clicar no botão ‘Ver pets disponíveis para adoção”', () => {
+      cy.visit('https://adopet-frontend-cypress.vercel.app/');
+      cy.get('.button').click()     
+      
+  })
+  it("Visita a página de principal do AdoPet e testa os botão de home", ()=>{
+      cy.visit('https://adopet-frontend-cypress.vercel.app/');
+      cy.get('.header__home').click()  
+
+  })
+  it("Visita a página de /login do Adopet", ()=>{
+    cy.visit('https://adopet-frontend-cypress.vercel.app/login');
+  })
+    it("Visita a página de /home do Adopet", ()=>{
+      cy.visit('https://adopet-frontend-cypress.vercel.app/home');
+  })
+    it("Visita a página de /home do AdoPet e clique no botão “Falar com o responsável”", ()=>{
+        cy.visit('https://adopet-frontend-cypress.vercel.app/home');
+        cy.get('.header__message').click()  
+    })
+    */
+});
+
