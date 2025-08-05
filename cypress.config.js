56509c2bcac27725188e6092563f6e32afab116c
Luciano Bohnert <luciano.bohnert@gmail.com>
2025-08-05 19:35:45 -0300

    Gerando relatórios com o mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,29 @@
 const { defineConfig } = require("cypress");
+const fs = require('fs');
 
 module.exports = defineConfig({
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          const hasFailures = results.tests.some(test =>
+            test.attempts.some(attempt => attempt.state === 'failed')
+          );
+
+          if (!hasFailures) {
+            // Deleta o vídeo se não houve falhas
+            fs.unlinkSync(results.video);
+          }
+        }
+      });
     },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: true,
+      timestamp: "mmddyyyy_HHMMss" }
   },
 });
--- cypress/e2e/cadastro-correto.cy.js
+++ cypress/e2e/cadastro-correto.cy.js
@@ -2,11 +2,13 @@ describe('Página de Cadastro', () => {
   it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
     cy.get('[data-test="register-button"]').click();
-    cy.get('[data-test="input-name"]').type('Ana de Jesus');
-    cy.get('[data-test="input-email"]').type('ana@email.com');
+    cy.get('[data-test="input-name"]').type('Ana de Jesus Neta');
+    cy.get('[data-test="input-email"]').type('ana.neta@email.com');
     cy.get('[data-test="input-password"]').type('Senha123');
     cy.get('[data-test="input-confirm-password"]').type('Senha123');
     cy.get('[data-test="submit-button"]').click();
+    cy.wait(3000); // espera 3 segundos
+    cy.contains('Falha ao cadastrar!').should('not.exist');
   })
   /*
   it('deve carregar a página corretamente e clicar no botão ‘Ver pets disponíveis para adoção”', () => {
