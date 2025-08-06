3bac444f344aaa008fbfd5451f776ef9eb085e80
Luciano Bohnert <luciano.bohnert@gmail.com>
2025-08-06 11:38:26 -0300

    Usando comandos personalizados para reutilizar código

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -3,9 +3,7 @@ describe('Página de login', () => {
       cy.visit('https://adopet-frontend-cypress.vercel.app/');
       cy.get('[data-test="login-button"]').click();
     })
-        it('Deve preencher os campos do login corretamente e autenticar o usuário na página', () => {
-          cy.get('[data-test="input-loginEmail"]').type('ana@email.com');
-          cy.get('[data-test="input-loginPassword"]').type('Senha123');
-          cy.get('[data-test="submit-button"]').click();
-        })
+    it('Deve preencher os campos do login corretamente e autenticar o usuário na página', () => {
+      cy.login('ana@email.com', 'Senha123');
+    })
 })
